c10a9343f7f1d8dd833f907d4b252ad0ee118520
Callum Boylan <2boylc17@solent.ac.uk>
2023-10-12 11:37:54 +0100

    yes

--- While loops.py
+++ While loops.py
@@ -4,7 +4,7 @@ removed = 0
 while removed != apples and apples >= 0:
     print(f"Removed {removed + 1} apples")
     removed = removed + 1
-'''
+
 
 print('How many bars should be charged?')
 bars = int(input())
@@ -12,3 +12,14 @@ charged = 0
 while charged != bars and bars >= 0:
     print(f'Charging:', '█ ' * (charged + 1))
     charged = charged + 1
+'''
+
+print("How many numbers should i sum up?")
+numbers = int(input())
+count = 0
+final = 0
+while count < numbers:
+    print(f"Please enter number {count + 1} of {numbers}")
+    final = final + int(input())
+    count = count + 1
+print(f"The answer is {final}")
